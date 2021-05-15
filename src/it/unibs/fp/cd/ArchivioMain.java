3e5a0413297f598b4da69826cd22c5314637e974
Brignoo <gianlu.brignoli@gmail.com>
2021-05-15 11:56:41 +0200

    --

--- src/it/unibs/fp/cd/ArchivioMain.java
+++ src/it/unibs/fp/cd/ArchivioMain.java
@@ -2,11 +2,17 @@ package it.unibs.fp.cd;
 import it.unibs.fp.mylib.MyMenu;
 
 public class ArchivioMain {
+	
+	private static final String VOCE1 = "Aggiungi CD";
+	private static final String VOCE2 = "Visualizza collezione"; 
+	private static final String VOCE3 = "Rimozione CD";
+	private static final String VOCE4 = "Selezione casuale";
+	
+	private static final String MENU[] = {VOCE1, VOCE2, VOCE3, VOCE4}; 
 
 	public static void main(String[] args) {
 		//creare cd
-		String voci[] = {"Aggiungi CD", "Visualizza collezione", "Rimozione CD", "Selezione casuale"};
-		MyMenu menu = new MyMenu("ARCHIVIO CD", voci);
+		MyMenu menu = new MyMenu("ARCHIVIO CD", MENU);
 		menu.stampaMenu();
         //visualizza collezione cd
         //rimozione cd
